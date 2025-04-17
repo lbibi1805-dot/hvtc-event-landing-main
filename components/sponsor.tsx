fa2da437fa0c4bc4d4014647145f386961149351
lbibi1805-dot <chibachluong@gmail.com>
2025-04-17 23:07:12 +0700

    logo_update

--- components/sponsor.tsx
+++ components/sponsor.tsx
@@ -266,22 +266,22 @@ export default function Sponsor() {
       </div>
 
       {/* BẢO TRỢ CHUYÊN MÔN*/}
-      {/* <div className="max-w-4xl font-semibold mx-auto text-center mb-8">
-        <h3 className="h3 mb-4" >BẢO TRỢ CHUYÊN MÔN</h3>
+      <div className="max-w-4xl font-semibold mx-auto text-center mb-20">
+        <h3 className="h3 lg:-mb-3 mb-4" >BẢO TRỢ CHUYÊN MÔN</h3>
 
-        <div className="max-w-sm mx-auto grid gap-10 lg:gap-10 items-start lg:max-w-none mb-10 lg:mb-0">
+        <div className="max-w-sm mx-auto grid gap-10 lg:grid-cols-2 lg:gap-40 items-start lg:max-w-none mb-10 lg:mb-0">
           <div
             className="flex flex-col p-2 justify-center items-center"
             data-aos="fade-up"
             data-aos-delay="400"
           >
             <img
-              // src="/images/2025/sponsor/btcm/LCD KHOA NGÂN HÀNG BẢO HIỂM.jpg"
+              src="/images/2025/sponsor/btcm/110e4712_bvsc.png"
               className="h-auto rounded-lg p-4 lg:mt-14"
               alt=""
-              height={380}
-              width={270}
-              style={{ maxWidth: "none", backgroundColor:"black" }}
+              height={600}
+              width={480}
+              style={{ maxWidth: "none", backgroundColor:"white" }}
             />
           </div>
           <div
@@ -290,35 +290,16 @@ export default function Sponsor() {
             data-aos-delay="400"
           >
             <img
-              src="/images/2025/sponsor/btcm/logo-ysvn-vie-color.png"
+              src="/images/2025/sponsor/btcm/IMG_4797.PNG"
               className="h-auto rounded-lg p-4 lg:mt-16"
               alt=""
-              height={1500}
-              width={760}
+              height={600}
+              width={400}
               style={{ maxWidth: "none", backgroundColor:"#fff" }}
             />
           </div>
         </div>
-      </div> */}
-      <div className="max-w-4xl font-semibold mx-auto text-center mb-8">
-        <h3 className="h3 mb-4" >BẢO TRỢ CHUYÊN MÔN</h3>
-
-        <div className="max-w-sm mx-auto grid gap-10 lg:grid-cols-2 lg:gap-10 items-start lg:max-w-none mb-10 lg:mb-0">
-          <div
-            className="flex flex-col p-2 justify-center items-center"
-            data-aos="fade-up"
-            data-aos-delay="400"
-          >
-            <img
-              src="/images/2025/sponsor/bttt/images.png"
-              className="h-auto rounded-lg p-4 lg:mt-14"
-              alt=""
-              height={380}
-              width={270}
-              style={{ maxWidth: "none", backgroundColor:"black" }}
-            />
-          </div>
-          <div
+        <div
             className="flex flex-col p-0 justify-center items-center"
             data-aos="fade-up"
             data-aos-delay="400"
@@ -327,17 +308,17 @@ export default function Sponsor() {
               src="/images/2025/sponsor/btcm/logo-ysvn-vie-color.png"
               className="h-auto rounded-lg p-4 lg:mt-16"
               alt=""
-              height={380}
-              width={380}
+              height={720}
+              width={540}
               style={{ maxWidth: "none", backgroundColor:"#fff" }}
             />
           </div>
-        </div>
+      
       </div>
 
       {/* BẢO TRỢ truyền thông */}
-      <div className="max-w-4xl font-semibold mx-auto text-center mb-8">
-        <h3 className="h3 mb-4" >BẢO TRỢ TRUYỀN THÔNG</h3>
+      <div className="max-w-4xl font-semibold mx-auto text-center mb-20">
+        <h3 className="h3 lg:-mb-5 mb-8" >BẢO TRỢ TRUYỀN THÔNG</h3>
 
         <div className="max-w-sm mx-auto grid gap-10 lg:grid-cols-2 lg:gap-10 items-start lg:max-w-none mb-10 lg:mb-0">
           <div
@@ -363,17 +344,73 @@ export default function Sponsor() {
               src="/images/2025/sponsor/bttt/Logo TĐM.jpg"
               className="h-auto rounded-lg p-4 lg:mt-16"
               alt=""
-              height={190}
-              width={190}
+              height={380}
+              width={270}
+              style={{ maxWidth: "none", backgroundColor:"#fff" }}
+            />
+          </div>
+          <div
+            className="flex flex-col p-2 justify-center items-center"
+            data-aos="fade-up"
+            data-aos-delay="400"
+          >
+            <img
+              src="/images/2025/sponsor/bttt/Picture1.png"
+              className="h-auto rounded-lg p-4 lg:mt-14"
+              alt=""
+              height={600}
+              width={540}
+              style={{ maxWidth: "none", backgroundColor:"white" }}
+            />
+          </div>
+          <div
+            className="flex flex-col p-0 justify-center items-center"
+            data-aos="fade-up"
+            data-aos-delay="400"
+          >
+            <img
+              src="/images/2025/sponsor/bttt/Picture2.png"
+              className="h-auto rounded-lg p-4 lg:mt-16"
+              alt=""
+              height={380}
+              width={270}
               style={{ maxWidth: "none", backgroundColor:"#fff" }}
             />
           </div>
         </div>
+        <div
+            className="flex flex-col p-2 justify-center items-center"
+            data-aos="fade-up"
+            data-aos-delay="400"
+          >
+            <img
+              src="/images/2025/sponsor/bttt/Picture3.png"
+              className="h-auto rounded-lg p-4 lg:mt-14"
+              alt=""
+              height={600}
+              width={480}
+              style={{ maxWidth: "none", backgroundColor:"white" }}
+            />
+        </div>
+        <div
+            className="flex flex-col p-0 justify-center items-center"
+            data-aos="fade-up"
+            data-aos-delay="400"
+          >
+            <img
+              src="/images/2025/sponsor/bttt/Picture4.png"
+              className="h-auto rounded-lg p-4 lg:mt-16"
+              alt=""
+              height={600}
+              width={480}
+              style={{ maxWidth: "none", backgroundColor:"#fff" }}
+            />
+          </div>
       </div>
 
       {/* Đối tác truyền hông */}
       <div className="max-w-4xl font-semibold mx-auto text-center mb-8">
-        <h3 className="h3 mb-4" >ĐỐI TÁC TRUYỀN THÔNG</h3>
+        <h3 className="h3" >ĐỐI TÁC TRUYỀN THÔNG</h3>
 
         <div className="max-w-sm mx-auto grid gap-10 lg:grid-cols-2 lg:gap-10 items-start lg:max-w-none mb-10 lg:mb-0">
           <div
@@ -437,7 +474,7 @@ export default function Sponsor() {
           </div>
         </div>
 
-        <div className="max-w-sm mx-auto grid gap-10 lg:grid-cols-2 lg:gap-10 items-start lg:max-w-none">
+        <div className="max-w-sm mx-auto grid gap-10 lg:grid-cols-2 lg:gap-10 items-start lg:max-w-none mb-10">
           <div
             className="flex flex-col p-2 justify-center items-center"
             data-aos="fade-up"
@@ -466,8 +503,8 @@ export default function Sponsor() {
               style={{ maxWidth: "none", backgroundColor:"#fff" }}
             />
           </div>
-
-          <div
+        </div>
+        <div
             className="flex flex-col p-0 justify-center items-center"
             data-aos="fade-up"
             data-aos-delay="400"
@@ -481,8 +518,6 @@ export default function Sponsor() {
               style={{ maxWidth: "none", backgroundColor:"#fff" }}
             />
           </div>
-        </div>
-
       </div>
     </section>
   );
